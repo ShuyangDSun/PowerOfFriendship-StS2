e7e2659a2b31f9b5b348f285367a982b5d41d481
Daniel Sun <shuyangdanielsun@gmail.com>
2026-08-27 15:23:40 -0400

    Added Combined Health Pool (and changed some settings)

---
+++ PowerOfFriendship/PowerOfFriendshipCode/OurHealthPatch.cs
@@ -0,0 +1,21 @@
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Runs;
+
+namespace PowerOfFriendship.PowerOfFriendshipCode;
+
+[HarmonyPatch(typeof(RunState), nameof(RunState.CreateForNewRun))]
+public class OurHealthPatch
+{
+    [HarmonyPostfix]
+    public static void Postfix(
+        RunState __result)
+    {
+        var totalHealth = __result.Players.Sum(player => player.Creature.MaxHp);
+
+        foreach (var player in __result.Players)
+        {
+            CreatureCmd.SetMaxAndCurrentHp(player.Creature, totalHealth);
+        }
+    }
+}
