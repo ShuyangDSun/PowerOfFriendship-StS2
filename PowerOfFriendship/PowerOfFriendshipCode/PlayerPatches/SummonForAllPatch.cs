f0193656000f199af97654a1349a816398e16d65
r43luo <r43luo@uwaterloo.ca>
2026-08-28 20:26:53 -0400

    fixes

--- PowerOfFriendship/PowerOfFriendshipCode/PlayerPatches/SummonForAllPatch.cs
+++ PowerOfFriendship/PowerOfFriendshipCode/PlayerPatches/SummonForAllPatch.cs
@@ -12,19 +12,19 @@ using MegaCrit.Sts2.Core.Models;
 
 namespace PowerOfFriendship.PowerOfFriendshipCode.PlayerPatches;
 [HarmonyPatch(
-    typeof(CreatureCmd),
-    nameof(Hook.AfterSummon))
+    typeof(OstyCmd),
+    nameof(OstyCmd.Summon))
 ]
 public class SummonForAllPatch
 {
-    [HarmonyPrefix]
-    private static void Postfix(ref Task<decimal> __result, PlayerChoiceContext choiceContext, Player summoner, Decimal amount, CardPlay cardPlay)
+    [HarmonyPostfix]
+    private static void Postfix(ref Task<decimal> __result, PlayerChoiceContext choiceContext, Player summoner, Decimal amount, AbstractModel source)
     {
         __result = PlayerSync.ApplyEffectToPlayers(__result, summoner.Creature, GainSummon);
         return;
         async Task<SummonResult> GainSummon(Creature target)
         {
-             SummonResult summonResult = await OstyCmd.Summon(choiceContext, summoner, amount, null);
+             SummonResult summonResult = await OstyCmd.Summon(choiceContext, summoner, amount, source);
              return summonResult;
         }
     }
